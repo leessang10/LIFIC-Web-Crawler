9a879e44fb07920141a0ab4fada987a2540d6aec
leessang10 <leessang10@naver.com>
2021-04-20 17:29:35 +0900

    주석 추가

--- 0x01_naver_map/glance.py
+++ 0x01_naver_map/glance.py
@@ -22,6 +22,7 @@ driver.implicitly_wait(3)
 cnt = 0
 for row in table:
     try:
+        # 업체 명, 업체 번호 출력
         store_name = row[0].value
         store_num = row[1].value
         print("==================================================")
@@ -35,7 +36,7 @@ for row in table:
         time.sleep(3)
         # Iframe 전환
         driver.switch_to.frame("entryIframe")
-
+        # 업체 정보가 나타내는 정보의 수준을 4단계로 구분하여 입력받음
         print("A. 업체 정보에 있음\n"
               "B. 업체 홈페이지에 있음\n"
               "C. 소개글만 있음\n"
@@ -43,16 +44,20 @@ for row in table:
         
         status = input()
         row[5].value = status
+
+        # 업체 정보 중 '간략한소개'을 개행없이 출력
         store_info = driver.find_element_by_class_name("_6aUG7")
         store_intro = store_info.find_element_by_class_name("_3__3i").click()
         intro = str(store_info.find_element_by_class_name("WoYOw").text)
         intro = intro.replace("\n", " ")
         print(intro)
 
+        # Iframe 전환
+        driver.switch_to.default_content()
     except:
         print("error")
     finally:
-        driver.switch_to.default_content()
+        # 무조건 엑셀파일 저장
         load_wb.save("C:\\Users\\leess\\Downloads\\lific.xlsx")
 
 
